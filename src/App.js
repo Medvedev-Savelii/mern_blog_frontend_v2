45c9535b0b044567203dcc582d59f3b8ccfcae1b
sava <savamedvedev@gmail.com>
2022-07-25 19:26:26 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Topbar from "./components/topbar/Topbar";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Topbar />
         <Routes>
-          <Route exact path="/" element={<Topbar />} />
+          <Route exact path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/components/posts/Posts.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./posts.css";
+
+export default function Posts() {
+  return (
+    <div className="posts">
+      <h1>Hell</h1>
+      <h1>Hell</h1>
+      <h1>Hell</h1>
+      <h1>Hell</h1>
+      <h1>Hell</h1>
+      <h1>Hell</h1>
+    </div>
+  );
+}
