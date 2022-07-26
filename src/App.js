e8a3632d845be23191ea03fd8ea939151fcc31a1
sava <savamedvedev@gmail.com>
2022-07-26 11:29:30 +0800

    third commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/home/Home";
-
+import Single from "./pages/single/Single";
 function App() {
   return (
     <>
@@ -10,6 +9,7 @@ function App() {
         <Topbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route path="/post/:id" element={<Single />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import Post from "../post/Post";
 import "./posts.css";
 
 export default function Posts() {
   return (
     <div className="posts">
-      <h1>Hell</h1>
-      <h1>Hell</h1>
-      <h1>Hell</h1>
-      <h1>Hell</h1>
-      <h1>Hell</h1>
-      <h1>Hell</h1>
+      <Post img={"https://pirozhki.spb.ru/photo/Iceland/_MG_0631-PSD-10.jpg"} />
+      <Post img={"https://pirozhki.spb.ru/photo/Iceland/_MG_0631-PSD-10.jpg"} />
+      <Post img={"https://pirozhki.spb.ru/photo/Iceland/_MG_0631-PSD-10.jpg"} />
+      <Post img={"https://pirozhki.spb.ru/photo/Iceland/_MG_0631-PSD-10.jpg"} />
+      <Post img={"https://pirozhki.spb.ru/photo/Iceland/_MG_0631-PSD-10.jpg"} />
+      <Post img={"https://pirozhki.spb.ru/photo/Iceland/_MG_0631-PSD-10.jpg"} />
     </div>
   );
 }
