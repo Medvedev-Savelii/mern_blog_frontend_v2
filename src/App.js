1a86339485a2b650e1b444422c337b87739098db
sava <savamedvedev@gmail.com>
2022-07-26 22:03:49 +0800

    six-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/home/Home";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
 
 function App() {
+  const currentUser = false;
   return (
-    <>
-      <BrowserRouter>
-        <Topbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/post/:id" element={<Single />} />
-          <Route path="/write" element={<Write />} />
-          <Route path="/settings" element={<Settings />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <Router>
+      <Topbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/posts">
+          <Home />
+        </Route>
+        <Route path="/register">{currentUser ? <Home /> : <Register />}</Route>
+        <Route path="/login">{currentUser ? <Home /> : <Login />}</Route>
+        <Route path="/post/:id">
+          <Single />
+        </Route>
+        <Route path="/write">{currentUser ? <Write /> : <Login />}</Route>
+        <Route path="/settings">{currentUser ? <Settings /> : <Login />}</Route>
+      </Switch>
+    </Router>
   );
 }
 
