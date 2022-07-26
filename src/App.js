30b18000b4ae6c91a23b7498455a202ea8f69dce
sava <savamedvedev@gmail.com>
2022-07-26 16:54:17 +0800

    four-commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/home/Home";
 import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+
 function App() {
   return (
     <>
@@ -10,6 +12,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/post/:id" element={<Single />} />
+          <Route path="/write" element={<Write />} />
         </Routes>
       </BrowserRouter>
     </>
